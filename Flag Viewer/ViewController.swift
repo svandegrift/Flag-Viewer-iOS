7ca36b573899bb7f123f6c7a3f140798f17c7a4b
Steven Vandegrift <svandegrift@icloud.com>
2020-08-12 20:57:02 -0500

    Initial Commit

---
+++ Flag Viewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flag Viewer
+//
+//  Created by Steven Vandegrift on 8/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
